02054fe812c9ced735a65a1011ef772dc8f32718
rajanvishwa10 <rajanvishwa10@gmail.com>
2020-08-19 19:14:52 +0530

    second commit

--- app/src/main/java/com/example/ttsapplication/SpeechToTextActivity.java
+++ app/src/main/java/com/example/ttsapplication/SpeechToTextActivity.java
@@ -10,6 +10,7 @@ import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -42,6 +43,35 @@ public class SpeechToTextActivity extends AppCompatActivity {
         if(requestCode == RECOGNIZER){
             ArrayList<String> match = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
             editText.setText(match.get(0).toString());
+            String demo = editText.getText().toString();
+            if(demo.equals("open youtube") || demo.equals("open Youtube") || demo.equals("open YouTube")){
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.google.android.youtube");
+                startActivity(intent);
+            }
+            else if(demo.equals("open whatsapp") || demo.equals("open Whatsapp") || demo.equals("open WhatsApp")){
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.whatsapp");
+                startActivity(intent);
+            }
+            else if(demo.equals("open Facebook")|| demo.equals("open facebook") || demo.equals("open FaceBook")){
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.facebook.katana");
+                startActivity(intent);
+            }
+            else if(demo.equals("open Instagram") || demo.equals("open instagram")){
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.instagram.android");
+                startActivity(intent);
+            }
+            else if(demo.equals("open Messenger") || demo.equals("open messenger")){
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.facebook.orca");
+                startActivity(intent);
+            }
+            else if(demo.equals("open snapchat") || demo.equals("open Snapchat")){
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.snapchat.android");
+                startActivity(intent);
+            }
+            else{
+                Toast.makeText(this, "App not installed or voice not reconized", Toast.LENGTH_LONG).show();
+            }
+
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
